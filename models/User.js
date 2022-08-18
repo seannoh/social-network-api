a796f3997d6206a0bfb1b4f5cae0b524c16622c7
Sean Oh <seanoh@ucsb.edu>
2022-08-18 09:57:13 -0700

    create user model

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const {Schema, model} = require("mongoose");
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            // regex courtesy of Manish Saraan (https://github.com/manishsaraan/email-validator)
+            match: /^[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~](\.?[-!#$%&'*+\/0-9=?A-Z^_a-z`{|}~])*@[a-zA-Z0-9](-*\.?[a-zA-Z0-9])*\.[a-zA-Z](-?[a-zA-Z0-9])+$/
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "thought"
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "user"
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        _id: false
+    }
+);
+
+userSchema.virtual("friendCount").get(function () {
+    return this.friends.length
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
