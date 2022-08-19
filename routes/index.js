d5e42985fc14e5940296a05f630aeeb91cc729f2
Sean Oh <seanoh@ucsb.edu>
2022-08-18 22:12:45 -0700

    set up route indexes

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const router = require("express").Router;
+const apiRoutes = require("./api");
+
+router.use("/api", apiRoutes);
+
+router.use((req, res) => res.status(400).json({message: "Invalid route"}));
+
+module.exports = router;
